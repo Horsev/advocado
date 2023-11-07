8adca278fadd1b8cb1a0ff519c8aa6a6778345f6
Horsev <andrey.horsev@gmail.com>
2023-11-07 20:46:58 +0200

    1st Poster Team added

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,8 @@
 export const MAPPERS = {
   "https://production.5nix.com/api/kpiSales/kpiCurrent": "sales-team",
   "https://production.5nix.com/api/kpiDev/kpiCurrent": "dev-team",
-  "https://posterpos.app.n8n.cloud/webhook/kpiDev": "poster-team",
+  "https://posterpos.app.n8n.cloud/webhook/kpiDev?team=Milky%20Way":
+    "poster-team-milky-way",
 };
 
 export default MAPPERS;
